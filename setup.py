8431938070e8c3a0d9e0896da000fc6abbd511b9
fxfitz <francis.x.fitzpatrick@gmail.com>
2016-01-05 18:17:00 -0600

    Plumbing

---
+++ setup.py
@@ -0,0 +1,20 @@
+import imp
+
+from setuptools import find_packages, setup
+
+VERSION = imp.load_source('autumn_init', './autumn/version.py').__version__
+
+
+setup(
+    name='autumn',
+    version=VERSION,
+    description="Internet File Harvester",
+    long_description=(
+        "Autumn is an file harvester that scours the internet for filetypes "
+        "and downloads them!"
+    ),
+    author="Fran Fitzpatrick",
+    author_email='francis.x.fitzpatrick@gmail.com',
+    license='GPL v2',
+    packages=find_packages(),
+)
